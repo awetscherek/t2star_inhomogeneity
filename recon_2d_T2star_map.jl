75cc778560db4e10c562f33d7192585c76447702
Dominic Qu <domqu5@gmail.com>
2025-02-23 21:01:50 +0000

    Fix dimension mismatches

--- recon_2d_T2star_map.jl
+++ recon_2d_T2star_map.jl
@@ -16,7 +16,7 @@ function recon_2d_t2star_map(config, kx, ky, raw, time_since_last_rf, dims; # ke
     #(269,8,536)
     # nky => necho => nkx
     nx, ny = dims;
-    nz = size(raw, 3) # this assumes only data from one echo is passed to the function, but several slices, so raw should be a 4D array
+    nz = size(raw, 4) # this assumes only data from one echo is passed to the function, but several slices, so raw should be a 4D array
 
     nkx, _, nky,_ = size(kx)
 
@@ -44,15 +44,20 @@ function recon_2d_t2star_map(config, kx, ky, raw, time_since_last_rf, dims; # ke
     # and use only data from central k-space region:
     selection = -pi .<= kx_d .< pi .&& -pi .<= ky_d .< pi;
 
+    println("selection shape")
+    println(size(selection))
+
     # this is where the coil images will be stored - note that we still will reconstruct several slices:
     t2_d = combine_coils ? Array{ComplexF64}(undef, nx, ny, nz) : Array{ComplexF64}(undef, nx, ny, nz, config["nchan"]);
     s0_d = combine_coils ? Array{ComplexF64}(undef, nx, ny, nz) : Array{ComplexF64}(undef, nx, ny, nz, config["nchan"]);
 
     # this is the raw data from which we want to reconstruct the coil images
     #(timepoints, ky, nz * nchan)
-    #Erroring here - need to change selection
-    y_d = reshape(ComplexF64.(permutedims(raw,[1 3 5 4 2])) .* sqrt.(dcf), nkx * config["necho"], :, nz * config["nchan"])[:,selection,:];
-    println(size(y_d))
+
+    println("Raw shape")
+    println(size(raw))
+
+    y_d = reshape(ComplexF64.(permutedims(raw,[1 3 5 4 2])) .* sqrt.(dcf), nkx * config["necho"], :, nz * config["nchan"])[selection, :];
 
     println("y_d shape")
     println(size(y_d))
@@ -82,13 +87,19 @@ function recon_2d_t2star_map(config, kx, ky, raw, time_since_last_rf, dims; # ke
     t2_d .= 0.0;
     s0_d .= 0.0;
 
-    #find shape of time_since_last_rf
+    time_since_last_rf = vec(time_since_last_rf)
+
+    println("time_since_last_rf")
     println(size(time_since_last_rf))
-    time_since_last_rf = vec(permutedims(time_since_last_rf, [4 3 1 2]))
 
     for t = 1:timepoints
-        kx_d_t = kx_d[:,t]
-        ky_d_t = kx_d[:,t]
+        kx_d_t = kx_d[t,;][selection[t,:]]
+        ky_d_t = ky_d[t,:][selection[t,:]]
+
+        println("kx_d_t dimensions")
+        println(size(kx_d_t))
+        println("ky_d_t dimensions")
+        println(size(ky_d_t))
 
         t_ms = time_since_last_rf[t]
 
@@ -96,9 +107,9 @@ function recon_2d_t2star_map(config, kx, ky, raw, time_since_last_rf, dims; # ke
 
         # plan NUFFTs:
         plan2 = finufft_makeplan(2, dims, 1, nz * config["nchan"], tol)     # type 2 (forward transform)
-        finufft_setpts!(plan2, kx_d_t[selection], ky_d_t[selection])
+        finufft_setpts!(plan2, kx_d_t, ky_d_t)
         plan1 = finufft_makeplan(1, dims, -1, nz * config["nchan"], tol)    # type 1 (adjoint transform)
-        finufft_setpts!(plan1, kx_d_t[selection], ky_d_t[selection])
+        finufft_setpts!(plan1, kx_d_t, ky_d_t)
 
         #patial derivatives of t2 and s0 w.r.t. w
         grad_s0_d = exp.(t_ms ./ t2_d)
@@ -106,9 +117,9 @@ function recon_2d_t2star_map(config, kx, ky, raw, time_since_last_rf, dims; # ke
 
         # calculate the residual
         
-        w_d_t = s0_d * Base.e ^ ((t_ms ./ t2_d))
+        w_d_t = s0_d .* exp.(t_ms ./ t2_d)
 
-        finufft_exec!(plan2, w_d_t .* c_d, r)
+        finufft_exec!(plan2, w_d_t .* c_d, r_t)
         r_t .*= dcf_d;
         r_t .-= y_d[t];
         obj = real(r_t[:]' * r_t[:]) / 2.0; # objective function
